d30419c673c2a21aec73ac49342e9ee62075d8ac
hojun <hojun@DESKTOP-HUKD7GE>
2017-05-10 12:42:09 +0900

    제출

---
+++ JavaPractice/src/org/dimigo/inheritance/PersonTest2.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.ingeritance
+ *	   |_ PersonTest
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 4. 26.
+ * <pre>
+ *
+ * @author hojun
+ * @version : 1.0
+ */
+public class PersonTest2 {
+
+	
+	public static void main(String[] args) {
+		
+		Person[] people = {
+			new Person("Tom"),
+			new Korean("홍길동"),
+			new Japanese("다나카"),
+			new Chinese("왕밍")
+		};
+		
+		for(Person person : people){
+			greeting(person);
+			
+		}
+		
+		
+		}
+	
+	private static void greeting(Person p){
+		System.out.println(p);
+		p.sayHello();
+		p.sayBye();
+		System.out.println();
+}
+
+}
