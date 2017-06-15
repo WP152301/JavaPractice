26f7dd22f66a463243c3d6a9c8f5384cdc8bd091
hojun <hojun@DESKTOP-HUKD7GE>
2017-06-15 20:07:07 +0900

    .

---
+++ JavaPractice/src/org/dmigo/io/ArakoMenu.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package org.dmigo.io;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.io.Writer;
+
+/**
+ * <pre>
+ * org.dmigo.io
+ *	   |_ ArakoMenu
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 6. 15.
+ * <pre>
+ *
+ * @author hojun
+ * @version : 1.0
+ */
+
+	public class ArakoMenu {
+	    public static void main(String[] args) {
+	        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+	            BufferedWriter writer = new BufferedWriter(new FileWriter("files/menu.txt"));
+	            BufferedReader reader = new BufferedReader(new FileReader("files/menu.txt"))){
+
+	            String data;
+	            while((data = bufferedReader.readLine()) != null) {
+	                writer.write(data);
+	                writer.newLine();
+	            }
+	            writer.close();
+	            System.out.println(" << 메뉴 출력 >>");
+	            while ((data = reader.readLine()) != null) {
+	                System.out.println(data);
+	            }
+
+	        } catch (Exception e){
+	            e.printStackTrace();
+	        }
+	    }
+	}
